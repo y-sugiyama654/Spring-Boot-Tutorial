71afc610afcc866079b94c5849c2101bebf9af8b
Yuta <y.sugiyama54@gmail.com>
2022-08-30 23:29:46 +0900

    サンプルルーティング追加

--- src/main/java/com/spring/tutorial/SpringBootTutorialApplication.java
+++ src/main/java/com/spring/tutorial/SpringBootTutorialApplication.java
@@ -1,13 +1,24 @@
 package com.spring.tutorial;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
+@RequiredArgsConstructor
+@RequestMapping("/v1")
 public class SpringBootTutorialApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootTutorialApplication.class, args);
+	}
+
+	@GetMapping(path={"test"})
+	public ResponseEntity<String> test() {
+		return ResponseEntity.ok("ok");
 	}
 
 }
